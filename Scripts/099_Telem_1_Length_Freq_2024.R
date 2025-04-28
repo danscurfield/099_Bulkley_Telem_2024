bf8d07dc402ba184f37626dce819cb3353be154c
danscurfield <dan.scurfield@gmail.com>
2025-04-28 22:31:42 +0000

    Updates to include 2024 tagging data

--- Scripts/099_Telem_1_Length_Freq_2024.R
+++ Scripts/099_Telem_1_Length_Freq_2024.R
@@ -1,6 +1,7 @@
 # Read in, format, and plot Bulkley tagging data
 # Created by Pete Moniz - winter 2023
 # Updated by Dan Scurfield - Jan 2024
+# updated by Dan Scurfield - Apr 2025
 
 # Initial Setup ---------------------------------------------------------------
 
@@ -10,6 +11,7 @@ rm(list=ls(all=TRUE))
 # Load packages
 library(tidyverse)
 library(IFRthemes)
+library(lubridate)
 
 # Make all times UTC and avoid auto re-display of timezone
 Sys.setenv(TZ = "UTC") 
@@ -17,31 +19,47 @@ Sys.setenv(TZ = "UTC")
 # Read In Data ----------------------------------------------------------------
 
 # Read in tagging data and format dates and times
-tagData <- read.csv("Data Input/099_Tag_Metadata_2023.csv", 
+tagData <- read.csv("Data Input/099_Tag_Metadata_2024.csv", 
                     header = TRUE, 
-                    stringsAsFactors = FALSE, na.strings = c("","unknown", "NA")) %>%
-  #mutate_all(~str_replace_all(., "unknown", "")) %>%
-  mutate(date = mdy(date),
-         tagDateTime = as.POSIXct(paste0(date, time, sep = " "), 
+                    stringsAsFactors = FALSE, na.strings = c("","unkn", "NA")) %>%
+  mutate(date = Date,
+         time = ("00:00:00"),
+         freq = Frequency,
+         code = Tag.Number,
+         sex = Gender,
+         forkLength = Length) %>%
+  #Make all sex data upper case
+  mutate(sex = str_to_upper(sex)) %>%
+  #Manually input tags with missing frequency data
+  mutate(freq = case_when(
+    code == "19" ~ 500,  #code 19 only detected on 149.500
+    code == "88" ~ 500,  #code 88 only detected on 149.500
+    code == "199" ~ 320,  #code 199 only detected on 149.320
+    code == "193" ~ 320,  #code 193 only detected on 149.320
+    code == "192" ~ 320,  #code 192 only detected on 149.320
+    code == "191" ~ 320,  #code 191 only detected on 149.320
+    code == "197" ~ 320,  #code 197 only detected on 149.320
+    code == "194" ~ 320,  #code 194 only detected on 149.320
+    TRUE ~ freq)) %>%
+  #Manually input date for tags with missing tagging date
+  # Tag 149.500 13
+  mutate(date = case_when(
+    code == "13" & freq == "500" ~ "2024-07-12", #earliest date of tagging
+    TRUE ~ date)) %>%
+  #Make tagDateTime and freqCode columns
+  mutate(tagDateTime = as.POSIXct(paste0(date, time, sep = " "), 
                                   format="%Y-%m-%d %H:%M"),
          freq = paste("149.", freq, sep = ""),
          freqCode = paste(freq, code, sep = " ")) %>%
   dplyr::select(tagDateTime, freqCode, sex, forkLength) %>%
-  # filter out recaptured fish
-  filter(!(tagDateTime == "2023-07-10 09:15:00" & freqCode == "149.48 41"),
-         !(tagDateTime == "2023-07-10 09:15:00" & freqCode == "149.48 42"),
-         !(tagDateTime == "2023-07-10 09:15:00" & freqCode == "149.48 45"),
-         !(tagDateTime == "2023-07-10 09:15:00" & freqCode == "149.48 46"),
-         !(tagDateTime == "2023-07-10 09:15:00" & freqCode == "149.48 45"),
-         !(tagDateTime == "2023-07-10 10:56:00" & freqCode == "149.48 50")) %>%
-  #filter out tags with missing data
-  filter(!(freqCode == "149.42 NA"),
-         !(freqCode == "149.48 3?"),
-         !(freqCode == "149.48 49?"),
-         !(freqCode == "149.48 NA")) %>%
+  # filter out recaptured fish - no recaptured fish in 2024
+  #filter out tags with missing data - solved by sleuthing detection data
   #filter our tags not assigned
   filter(!(is.na(tagDateTime)))  %>%
   distinct(freqCode, .keep_all = TRUE) 
+
+#Save cleaned tag data
+write_csv(tagData, "Data Input/tagData.csv")
 
 # Quick Summaries -------------------------------------------------------------
 
@@ -59,8 +77,8 @@ tagData %>% group_by(sex) %>%
 # on top of each other.
 
 freqPlot <- tagData %>%
-  mutate(sex = case_when(sex == "m" ~ "Male",
-                         sex == "f" ~ "Female", 
+  mutate(sex = case_when(sex == "M" ~ "Male",
+                         sex == "F" ~ "Female", 
                          is.na(sex) ~ "Unknown")) %>%
   ggplot(aes(x = forkLength,
              # use the line below to remove the annoying borders on bins with no data
@@ -78,7 +96,7 @@ freqPlot <- tagData %>%
 freqPlot
 
 ggsave(plot = freqPlot,
-       "99_FreqPlot_2023.png",
+       "99_FreqPlot_2024.png",
        path = "Figures and Tables/",
        height = 4, width = 6.5, units = "in", dpi = 600)
 
