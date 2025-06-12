ec5ad09a221fb209fa345299fbbd41c1d07dbec2
danscurfield <dan.scurfield@gmail.com>
2025-06-12 17:59:22 +0000

    Updated code - no longer removing "lost tags" (13s burst) as most are obserbed upstream at a later date.

--- Scripts/099_Telem_2_Initial_Telem_Data_Cleaning_2024.R
+++ Scripts/099_Telem_2_Initial_Telem_Data_Cleaning_2024.R
@@ -1,6 +1,7 @@
 # Read in, format, and run initial filtering of mobile and fixed telemetry data
 # Created by Pete Moniz - winter 2023
-# Updated by Dan Scurfield - Feb 2024
+# Major updates by Dan Scurfield - Feb 2024
+# Updated by Dan Scurfield - June 2025
 
 # Initial Setup ---------------------------------------------------------------
 
@@ -12,6 +13,7 @@ library(tidyverse)
 library(IFRthemes)
 library(lubridate)
 library(dplyr)
+library(purrr)
 
 # Make all times UTC and avoid auto re-display of timezone
 Sys.setenv(TZ = "UTC") 
@@ -30,7 +32,7 @@ uploadOrion <- function(path) {
   
   tempdat <- map2(tempfile, basename(tempfile), ~ {
     read.table(.x, header = TRUE, stringsAsFactors = FALSE) %>%
-      mutate(File = .y)
+      mutate(file = .y)
   }) %>%
     reduce(rbind) %>%
     mutate(Freq = format(Freq, nsmall = 3),
@@ -41,7 +43,7 @@ uploadOrion <- function(path) {
            freqCode = paste(Freq, code, sep = " ")) %>%
     distinct(freqCode, dateTime, .keep_all = TRUE) %>%
     filter(date >= "2023-07-04") %>%
-    dplyr::select(date, dateTime, station, freqCode, code = Code, power = Power, File)
+    dplyr::select(date, dateTime, station, freqCode, code = Code, power = Power, file)
 }
 
 #Function to read-in SRX1200 or SRX800 data
@@ -162,7 +164,8 @@ mobileData <- srxUpload(path = "Data Input/Radio Downloads/Mobile") %>%
          dateTime = datetime,
          code = TagID,
          power = Power, 
-         station = Station, 
+         station = Station,
+         file = File,
          latitude = Latitude,
          longitude = Longitude) %>%
   mutate(frequency = case_when( #convert channel numbers to frequency values
@@ -171,18 +174,42 @@ mobileData <- srxUpload(path = "Data Input/Radio Downloads/Mobile") %>%
     Channel == 3 ~ "149.340",
     TRUE ~ "other")) %>%
   mutate(freqCode = paste(frequency, code, sep = " ")) %>% #cr4eate frewqcode column among others. 
-  dplyr::select(date, dateTime, station, freqCode, code, power, latitude, longitude)
+  dplyr::select(date, dateTime, station, freqCode, code, power, file, latitude, longitude)
          
   
   #Unfortunately it appears the mobile scans were not monitoring the 149.340 frequency
 
 # Read in fixed station data, combine, and add location and rkm info
-station1 <- uploadOrion(path = "Data Input/Radio Downloads/Station1")
-station2 <- uploadOrion(path = "Data Input/Radio Downloads/Station2")
-station4 <- uploadOrion(path = "Data Input/Radio Downloads/Station4")
-station5 <- uploadOrion(path = "Data Input/Radio Downloads/Station5")
-station6 <- uploadOrion(path = "Data Input/Radio Downloads/Station6") %>%
-  mutate(station = 6)
+station1 <- uploadOrion(path = "Data Input/Radio Downloads/Station1") #2024-07-04 to 2024-09-09
+station2 <- uploadOrion(path = "Data Input/Radio Downloads/Station2") #2024-07-04 to 2024-10-22
+station4 <- uploadOrion(path = "Data Input/Radio Downloads/Station4") #2024-07-05 to 2024-09-10
+station5 <- uploadOrion(path = "Data Input/Radio Downloads/Station5") #2024-07-05 to 2024-08-12 - data gap
+station6 <- uploadOrion(path = "Data Input/Radio Downloads/Station6") #2024-07-05 to 2024-09-27 
+
+#Error in the data discovered
+#There is only one download for station 5 with zero detections: bulkley_station_5_10172024.txt
+#Station 5 data is found in file bulkley_station_1_08122024.txt 2024-07-05 to 2024-08-12
+#There is data from station 1 in file bulkley_station_1_08142024.txt from 2024-07-04 to 2024-08-14
+#this makes me beleive the data labeled site 5 in bulkley_station_1_08122024.txt is correct
+#this means there is missing data from 2024-08-12 to 2024-10-24.
+#there may be a misnamed file with detection data from 2024-08-12 to 2024-10-24. 
+#this file could be named like bulkley_station_1_08122024.txt but with 5's in 
+#the Site column, the first 151 detections on this reader say site 5, until it 
+#was changed in the field July 5, 2024, making the misnaming easy.
+
+#Only Station 1 & 2 are in frequency 149.340
+#10 tags on this frequency
+#149.340 001
+#149.340 002
+#149.340 003
+#149.340 004
+#149.340 005
+#149.340 006
+#149.340 006
+#149.340 007
+#149.340 008
+#149.340 009
+#149.340 010
 
 # Bind and format to match mobile data
 ## Remove test tag data to not skew detection accuracy calculation
@@ -204,37 +231,80 @@ fixedData <- rbind(station1, station2, station4, station5, station6) %>%
                          station == "6" ~ 201),
          method = "Fixed")
 
-# rm(station1, station2, station6, station5, station4)
+fixedData %>%
+  distinct(freqCode)
 
-# Mobile Tracking Data Filtering ----------------------------------------------
+#remove unneeded items from environment
+rm(station1, station2, station6, station5, station4)
 
-
-mobileDataCleaned <- mobileData %>% # Started with 9538 detections
-  left_join(tagData, by = "freqCode", relationship = "many-to-many")  %>%
-  filter(!(is.na(tagDateTime))) %>% # Remove detections with no tag info. Down to 1382
-  filter(dateTime > tagDateTime) %>% # Remove detections before tag date and time (none). Still 1382
-  filter(!(is.na(lat))) %>% # Remove detections with no lat, long. Down to 1080
-  arrange(freqCode, dateTime) %>%
-  filter(!(freqCode == lag(freqCode) & dateTime == lag(dateTime))) %>% # Remove repeated detections. Down to 659
-  dplyr::select(-freq) # Remove freq column to reduce redundancy
-
-# Write out csv of cleaned mobile tracking data
-# Will assign rkms, combine with fixed station data, and clean further.
-write.csv(mobileDataCleaned, 
-          file = "Data Output/099_MobileTrackingData_1_InitialClean_2023.csv")
 
 # Fixed Station Data Filtering ------------------------------------------------
-fixedDataCleaned <- fixedData %>% #Started with 302,443 detections
+fixedDataCleaned <- fixedData %>% #Started with 1,687,017 detections
   left_join(tagData, by = "freqCode", relationship = "many-to-many")  %>%
-  filter(!(is.na(tagDateTime))) %>% # Remove detections with no tag info. Down to 220,551
-  filter(dateTime > tagDateTime) # Remove detections before tag date and time. Down to 164,539
+  filter(!(is.na(tagDateTime))) %>% # Remove detections with no tag info - 474,942 detections
+  filter(dateTime > tagDateTime) # Remove detections before tag date and time - 465,634 detections
 
 
 
 # Write out csv of initial clean fixed station data.
 # Will combine with mobile data after rkms are assigned and cleaned further.
 write.csv(fixedDataCleaned, 
-          file = "Data Output/099_FixedStationData_InitialClean_2023.csv")
+          file = "Data Output/099_FixedStationData_InitialClean_2024.csv")
+
+
+# Mobile Tracking Data Filtering ----------------------------------------------
+
+#initial clean
+
+mobileDat0 <- mobileData %>% # Started with 563 detections
+  left_join(tagData, by = "freqCode", relationship = "many-to-many")  %>%
+  # mutate(dateTime = as.POSIXct(date),
+  #        tagDateTime = as.POSIXct(tagDateTime)) %>%
+  filter(!(freqCode == "149.500 211")) %>% #removed test tag - 304 detections
+  filter(!(is.na(tagDateTime))) %>% # Remove detections with no tag info - 275 detections
+  filter(dateTime >= tagDateTime) %>% # Remove detections before tag date and time (none) - 275 detections
+  #filter(!(is.na(lat))) %>% # Remove detections with no lat, long. Down to 1080
+  arrange(dateTime, freqCode) %>%
+  mutate(dateTime = as.POSIXct(dateTime)) %>%
+  # filter(!(freqCode == lag(freqCode) & dateTime == lag(dateTime))) %>% # Remove repeated detections. Down to 659
+  dplyr::select(-c("date", "file")) #Remove date and file column
+
+#second clean
+##include lat/long for detections with NA's by assigning nearest values in time
+
+# Create a separate table with known lat/lon
+known_locs <- mobileDat0 %>%
+  filter(!is.na(latitude) & !is.na(longitude)) %>%
+  select(dateTime, latitude, longitude)
+
+# Helper function to find nearest known location
+get_nearest_location <- function(dt, known_locs) {
+  # Find the row in known_df with minimum time difference
+  nearest <- known_locs %>%
+    slice(which.min(abs(difftime(dateTime, dt, units = "secs"))))
+  return(nearest %>% select(latitude, longitude))
+}
+
+mobileDat1 <- mobileDat0 %>%
+  rowwise() %>%
+  mutate(
+    nearest = if (is.na(latitude) | is.na(longitude)) list(get_nearest_location(dateTime, known_locs)) else list(NULL),
+    latitude = if (is.na(latitude)) nearest$latitude else latitude,
+    longitude = if (is.na(longitude)) nearest$longitude else longitude
+  ) %>%
+  ungroup() %>%
+  select(-nearest)
+
+mobileDataCleaned <- mobileDat1
+
+#remove unneeded items in environment
+rm(known_locs, mobileDat0, mobileDat1)
+
+
+# Write out csv of cleaned mobile tracking data
+# Will assign rkms, combine with fixed station data, and clean further.
+write.csv(mobileDataCleaned, 
+          file = "Data Output/099_MobileTrackingData_1_InitialClean_2024.csv")
 
 
 # False Positive Rates --------------------------------------------------------
@@ -246,19 +316,19 @@ fixedData %>%
   count(station)
 
 # Lower Bulkley
-fixedDataCleaned %>% filter(station == "1") %>% count() # 130228 remaining of 267184 (48.7%); so 51.3% removed.
+fixedDataCleaned %>% filter(station == "1") %>% count() # 33324 remaining of 40170 (83.0%); so 17.0% removed.
 
 # Lower Bulkley
-fixedDataCleaned %>% filter(station == "2") %>% count() # 12215 remaining of 13054 (93.6%); so 6.4% removed.
+fixedDataCleaned %>% filter(station == "2") %>% count() # 305290 remaining of 1513306 (20.2%); so 79.8% removed.
 
 # Morice Lake Outlet
-fixedDataCleaned %>% filter(station == "6") %>% count() # 1 remaining of 6 (16.7%); so 83.3% removed.
+fixedDataCleaned %>% filter(station == "6") %>% count() # 64286 remaining of 70059 (91.8%); so 8.2% removed.
 
 # Nanika River
-fixedDataCleaned %>% filter(station == "4") %>% count() # 21467 remaining of 21571 (99.9); so 0.1% removed.
+fixedDataCleaned %>% filter(station == "4") %>% count() # 61682 remaining of 62402 (98.8); so 1.2% removed.
 
 # Atna River
-fixedDataCleaned %>% filter(station == "5") %>% count() # 628 remaining of 628 (100%); so 0% removed.
+fixedDataCleaned %>% filter(station == "5") %>% count() # 1052  remaining of 1080 (97.4%); so 2.6% removed.
 
 
 # Additional Stats for recommendations ------------------------------------
@@ -269,28 +339,59 @@ mobileDataCleanedLagTimes <- mobileDataCleaned %>%
   arrange(dateTime) %>%
   group_by(freqCode) %>%
   mutate(lag = difftime(dateTime, lag(dateTime), units = "secs"))
-# There are none.
+# 13s tags
+# 149.500 008 at 2024-10-01 17:58:23
+# 149.500 025 at 2024-10-01 17:59:17 (2x)
+
+#26s tags
+# 149.500 071 at 2024-10-01 19:29:55
+# 149.500 033 at 2024-10-01 19:38:50
+# 149.500 038 at 2024-10-01 19:30:59
 
 fixedDataCleanedLagTimes <- fixedDataCleaned %>%
   arrange(dateTime) %>%
   group_by(freqCode) %>%
   mutate(lag = difftime(dateTime, lag(dateTime), units = "secs")) 
-  
+
+
 lostTags <- fixedDataCleanedLagTimes %>%
   filter(lag == 13)  %>%
   dplyr::select(!(lag)) %>%
   summarize(unique_freqCode = n_distinct(freqCode)            ,
             earliestDate = min(dateTime))
-#18 lost tags detected on fixed stations
+
+#15 lost tags detected on fixed stations
+#I am going to look at the fixed data to see if they are seen upstream and 
+#determine whether they are lost (or dead) or alive fish.
+#alive
+#	149.320 127 seen upstream
+# 149.340 001 seen upstream (S2)
+# 149.340 002 seen upstream (S2)
+# 149.340 003 seen upstream (S2)
+# 149.340 004 seen upstream (S2)
+# 149.340 006 seen upstream (S2)
+# 149.340 008 seen upstream (S2)
+# 149.500 001 seen upstream (S5 Atna)
+# 149.500 002 seen upstream (S5 Atna)
+# 149.500 018 seen upstream (S4 Nanika)
+# 149.500 034 seen upstream (S6 Morice L)
+# 149.500 044 seen upstream (S4 Nanika)
+# 149.500 071 seen upstream (S4 Nanika)
+
+
+
+#lost (dead)
+#149.320 146
+#149.500 100 only seen at S2 since 2024-08-27
+
 
 write.csv(lostTags, 
-          file = "Data Output/099_LostTags_2023.csv")
+          file = "Data Output/099_LostTags_2024.csv")
 
-#this is probably because scanning multiple frequencies - can pick up on 13s intervals. 
+#I'm not going to rule out these fish entirely because many of them are later
+#detected upstream - meaning the "death signal" may have failed. 
 
-# Number of frequencies scanned per mobile survey. This should only be 2 but was 3 for
-# most surveys. Lots of wasted scan time :(
-freqMobileData <- mobileData %>%
-  group_by(date) %>%
-  count(freq)
-  
+# the mobile survey recorded data on 2 frequncies only:
+# 149.500 & 149.320
+
+# tags also put out in 149.340
